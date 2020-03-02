e1c410afbd77d7942665b4e7454a9c0e2f28acbb
Tanguy Culerier <culerier.tanguy@gmail.com>
2020-03-02 10:57:33 +0100

    init

---
+++ 3_3/src/main/java/fr/uvsq/uvsq21601979/_3/robot.java
@@ -0,0 +1,19 @@
+package fr.uvsq.uvsq21601979._3;
+
+public class robot
+{
+	private Position position;
+	private Direction direction;
+	
+	public robot()
+	{
+		
+	}
+	
+	public void tourne()
+	{
+		
+	}
+	
+	public void avance()
+}
---
+++ 3_3/src/main/java/fr/uvsq/uvsq21601979/_3/robotStatique.java
@@ -0,0 +1,6 @@
+package fr.uvsq.uvsq21601979._3;
+
+public class robotStatique extends robot
+{
+	
+}
