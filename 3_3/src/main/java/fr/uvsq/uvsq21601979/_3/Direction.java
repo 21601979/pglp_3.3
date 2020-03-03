553b4f22e81a8ac59d8d0d06aa9a890d0804a849
Tanguy Culerier <culerier.tanguy@gmail.com>
2020-03-03 22:12:13 +0100

    ajout des classe Position et Direction

---
+++ 3_3/src/main/java/fr/uvsq/uvsq21601979/_3/Direction.java
@@ -0,0 +1,11 @@
+package fr.uvsq.uvsq21601979._3;
+
+public class Direction 
+{
+	int direction;
+	
+	Direction(int direction)
+	{
+		this.direction = direction;
+	}
+}
---
+++ 3_3/src/main/java/fr/uvsq/uvsq21601979/_3/Position.java
@@ -0,0 +1,13 @@
+package fr.uvsq.uvsq21601979._3;
+
+public class Position 
+{
+	int x;
+	int y;
+	
+	Position(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
